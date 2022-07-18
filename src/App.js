758e0558096aa7a53cf0f496caeb2c2592532c47
pranav s <dev.pranabsk@gmail.com>
2022-07-18 22:23:26 +0530

    HotAccessories component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
 import Components from './components/Components';
-import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
       <Components/>
-    </Router>
   );
 }
 
--- src/components/Components.js
+++ src/components/Components.js
@@ -5,11 +5,14 @@ import Slider from './slider/Slider';
 import Ads from './ads/Ads';
 import Heading from './heading/Heading';
 import StarProducts from './starProducts/starProducts';
+import HotAccessoriesMenu from './HotAccessories/HotAccessoriesMenu';
+import HotAccessories from './HotAccessories/HotAccessories';
 import data from '../data/data.json';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 const Component = () => {
     return (
-        <div>
+        <Router>
             <PreNavbar/>
             <Navbar/>
             <Slider start={data.banner.start}/>
@@ -17,7 +20,30 @@ const Component = () => {
             <Heading text="STAR PRODUCTS"/>
             <StarProducts starProduct={data.starProduct}/>
             <Heading text="HOT ACCESSORIES"/>
-        </div>
+            <HotAccessoriesMenu/>
+         
+            <Route exact path='/music'>
+                <HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music}/>
+            </Route>
+
+            <Route exact path='/smartDevice'>
+                <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice}/>
+            </Route>
+            
+            <Route exact path='/home'>
+                <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home}/>
+            </Route>
+
+            <Route exact path='/lifestyle'>
+                <HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifestyleCover={data.hotAccessoriesCover.lifeStyle}/>
+            </Route>
+
+            <Route exact path='/mobileAccessories'>
+                <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}/>
+            </Route>
+
+
+        </Router>
     );
 };
 
