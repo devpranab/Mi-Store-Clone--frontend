2a104e44fb8fe646182938a1d2599a5d20b85739
pranav s <dev.pranabsk@gmail.com>
2022-07-18 17:07:59 +0530

    Heading component

--- src/components/Components.js
+++ src/components/Components.js
@@ -3,6 +3,7 @@ import PreNavbar from './navbar/PreNavbar';
 import Navbar from './navbar/Navbar';
 import Slider from './slider/Slider';
 import Ads from './ads/Ads';
+import Heading from './heading/Heading';
 import data from '../data/data.json';
 
 const Component = () => {
@@ -12,6 +13,7 @@ const Component = () => {
             <Navbar/>
             <Slider start={data.banner.start}/>
             <Ads offer={data.offer}/>
+            <Heading text="STAR PRODUCTS"/>
         </div>
     );
 };
